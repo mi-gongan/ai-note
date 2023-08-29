dd5c28d24b8bc54435318c1c3714ad8c6ca66438
mi-gongan <kib0202@naver.com>
2023-08-29 23:27:56 +0900

    ✨ feat: publishing

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,22 +1,36 @@
+"use client";
+
 import { cls } from "@/utils/style";
 import React from "react";
+import { motion } from "framer-motion";
 
 interface CustomButtonProps {
   text: string;
+  handleClick: () => void;
 }
 
-function CustomButton({ text }: CustomButtonProps) {
+function CustomButton({ text, handleClick }: CustomButtonProps) {
   return (
-    <div
+    <motion.div
+      whileHover={{
+        scale: 1.04,
+      }}
+      whileTap={{
+        scale: 0.96,
+      }}
+      onClick={handleClick}
       className={cls(
-        "bg-primary w-[240px] h-[77px] rounded-[64px]",
         "flex justify-center items-center",
-        "text-white text-[30px]",
+        "bg-primary md:w-[224px] md:h-[70px] md:rounded-[64px]",
+        "w-[180px] h-[52px] rounded-[40px]",
+        "md:text-[30px]",
+        "text-[20px]",
+        "text-white",
         "cursor-pointer"
       )}
     >
       {text}
-    </div>
+    </motion.div>
   );
 }
 
---
+++ src/components/modal/CustonModal.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Portal from "./Portal";
+import { cls } from "@/utils/style";
+import { motion, AnimatePresence } from "framer-motion";
+
+interface CustonModalProps {
+  children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+  width?: string;
+  borderRadius?: string;
+}
+
+function CustonModal({
+  children,
+  open,
+  onClose,
+  width,
+  borderRadius,
+}: CustonModalProps) {
+  return (
+    <Portal>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="top-0 left-0 right-0 bottom-0 fixed bg-black/50 w-full h-full flex justify-center items-center"
+            onClick={(e) => {
+              if (e.target === e.currentTarget) onClose();
+            }}
+          >
+            <div
+              className={cls(
+                "absolute bg-white z-2 max-h-[90vh] overflow-hidden",
+                width ? width : "w-[400px]",
+                borderRadius ? borderRadius : "rounded-[10px]"
+              )}
+            >
+              {children}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </Portal>
+  );
+}
+
+export default CustonModal;
