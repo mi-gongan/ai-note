009622a2b6786c11bddd82672d87840313c6186d
mi-gongan <kib0202@naver.com>
2023-09-02 04:24:32 +0900

    ✨ feat: 파일 확장자 제한

--- src/components/user/atom/FileDropzone.tsx
+++ src/components/user/atom/FileDropzone.tsx
@@ -12,7 +12,20 @@ function FileDropzone() {
 
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
-      dispatch(uploadFile(acceptedFiles[0]));
+      const selectedFile = acceptedFiles[0];
+      if (acceptedFiles) {
+        const allowedExtensions = [".mp3", ".mp4"];
+        const fileExtension = selectedFile.name.slice(
+          ((selectedFile.name.lastIndexOf(".") - 1) >>> 0) + 2
+        );
+        if (allowedExtensions.includes("." + fileExtension)) {
+          dispatch(uploadFile(acceptedFiles[0]));
+        } else {
+          alert(
+            "올바른 파일 확장자가 아닙니다. 허용된 확장자는 .mp3 및 .mp4 입니다."
+          );
+        }
+      }
     },
     [dispatch]
   );
@@ -28,7 +41,7 @@ function FileDropzone() {
         "flex justify-center items-center"
       )}
     >
-      <input {...getInputProps()} />
+      <input {...getInputProps()} accept=".mp3,.mp4" />
       <div className="flex justify-center items-center flex-col gap-3">
         <UploadIcon />
         <div className="text-[#9A9B9C] font-[600] md:text-[18px] text-[14px]">
